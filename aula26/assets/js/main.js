6b17f0e3cb26b21867e94da76006c15ae8d64391
Ad0rn0 <ad0rn0code@gmail.com>
2025-01-12 11:42:15 -0300

    Finished exercise class 26

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -1,10 +1,56 @@
-//IMC = peso (em kg) / altura^2 (em m)
-
-function calcularIMC() {
-    var resultado = document.querySelector("p#resultado");
+function calcularIMC(event) {
+    event.preventDefault();
+    var resultadoTrue = document.querySelector("p#resultadoTrue");
+    var resultadoFalse = document.querySelector("p#resultadoFalse");
     var peso = Number(document.getElementById("peso").value);
     var altura = Number(document.getElementById("altura").value);
     var imc = peso / (altura ** 2);
 
-    resultado.innerHTML = "Seu IMC é: " + imc.toFixed(2);
+    resultadoTrue.innerHTML = "";
+    resultadoTrue.style.padding = "";
+
+    resultadoFalse.innerHTML = "";
+    resultadoFalse.style.padding = "";
+
+    if (peso === 0 || altura === 0) {
+        resultadoFalse.style.padding = "10px";
+        resultadoFalse.innerHTML = "Preencha os campos corretamente!";
+        return;
+    }
+    else if (peso > 600 || peso < 10 || altura < 0 || altura > 3) {
+        resultadoFalse.style.padding = "10px";
+        resultadoFalse.innerHTML = "Valores inválidos!";
+        return;
+    }
+
+    if (imc < 18.5) {
+        resultadoTrue.style.padding = "10px";
+        resultadoTrue.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Abaixo do peso)";
+        return;
+    }
+    else if (imc >= 18.5 && imc <= 24.9) {
+        resultadoTrue.style.padding = "10px";
+        resultadoTrue.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Peso normal)";
+        return;
+    }
+    else if (imc >= 25 && imc <= 29.9) {
+        resultadoTrue.style.padding = "10px";
+        resultadoTrue.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Sobrepeso)";
+        return;
+    }
+    else if (imc >= 30 && imc <= 34.9) {
+        resultadoTrue.style.padding = "10px";
+        resultadoTrue.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Obesidade 1)";
+        return;
+    }
+    else if (imc >= 35 && imc <= 39.9) {
+        resultadoTrue.style.padding = "10px";
+        resultadoTrue.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Obesidade 2)";
+        return;
+    }
+    else if (imc >= 40) {
+        resultadoTrue.style.padding = "10px";
+        resultadoTrue.innerHTML = "Seu IMC é: " + imc.toFixed(2) + " (Obesidade 3)";
+        return;
+    }
 }
